896ff34b3f545e7e1d18ca0dc8b22fa866242eb2
Leo Bayoumi <Leo.Bayoumi0@gmail.com>
2025-11-03 05:30:33 +0200

    Add loading spinner for better UX

--- app.py
+++ app.py
@@ -21,21 +21,24 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded X-ray Image.', use_column_width=True)
     st.write("")
-    st.write("Classifying...")
+    
+    #st.write("Classifying...")
 
-    image = image.resize((128, 128), Image.LANCZOS)
-    image = image.convert('RGB')
+    with st.spinner('Classifying...'):
 
-    img_array = np.array(image)
+        image = image.resize((128, 128), Image.LANCZOS)
+        image = image.convert('RGB')
 
-    img_array = np.expand_dims(img_array, axis=0)
+        img_array = np.array(image)
 
-    prediction = model.predict(img_array)
-    score = prediction[0][0]
+        img_array = np.expand_dims(img_array, axis=0)
+
+        prediction = model.predict(img_array)
+        score = prediction[0][0]
 
 
-    # Display the result
-    if score > 0.5:
-        st.error(f"**Prediction: PNEUMONIA** (Confidence: {score * 100:.2f}%)")
-    else:
-        st.success(f"**Prediction: NORMAL** (Confidence: {(1 - score) * 100:.2f}%)")
+        # Display the result
+        if score > 0.5:
+            st.error(f"**Prediction: PNEUMONIA** (Confidence: {score * 100:.2f}%)")
+        else:
+            st.success(f"**Prediction: NORMAL** (Confidence: {(1 - score) * 100:.2f}%)")
